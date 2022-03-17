ea816187296c82d715f2f37c04e7d35dc551ea83
Kevin Tan <62129747+kevintsq@users.noreply.github.com>
2022-03-17 20:05:35 +0800

    Update GetProcessCPUTimeOnWindows.py

--- GetProcessCPUTimeOnWindows.py
+++ GetProcessCPUTimeOnWindows.py
@@ -12,20 +12,20 @@ from win32 import win32process
 class Runner(Thread):
     def run(self):
         pythoncom.CoInitialize()
-        c = wmi.WMI()
+        wmi_service = wmi.WMI()
         start_time = time.time()
-        with subprocess.Popen(r"java -jar Test.jar") as p:
-            result = c.query(f"SELECT * FROM Win32_Process WHERE ParentProcessId = {p.pid}")
-            while len(result) == 0 and not p.poll():
-                result = c.query(f"SELECT * FROM Win32_Process WHERE ParentProcessId = {p.pid}")
+        with subprocess.Popen(r"java -jar Test.jar") as process:
+            process_info = wmi_service.query(f"SELECT * FROM Win32_Process WHERE ParentProcessId = {process.pid}")
+            while len(process_info) == 0 and not process.poll():
+                process_info = wmi_service.query(f"SELECT * FROM Win32_Process WHERE ParentProcessId = {process.pid}")
             try:
-                handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, False, result[0].ProcessId)
-                p.wait()
-                result = win32process.GetProcessTimes(handle)
-                print((result['KernelTime'] + result['UserTime']) / 10000000)
+                handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, False, process_info[0].ProcessId)
+                process.wait()
+                process_info = win32process.GetProcessTimes(handle)
+                print((process_info['KernelTime'] + process_info['UserTime']) / 10000000)
                 win32api.CloseHandle(handle)
             except:
-                print((int(result[0].KernelModeTime) + int(result[0].UserModeTime)) / 10000000)
+                print((int(process_info[0].KernelModeTime) + int(process_info[0].UserModeTime)) / 1000)
             print(time.time() - start_time)
 
 
